4679b9b0e0c3c533d00921cdfacd7585706525a6
Imran Khan <ikhan@thoughtworks.com>
2017-12-11 23:03:09 +1100

    Added basic controller and test

---
+++ src/main/java/com/metlife/producer/Application.java
@@ -0,0 +1,26 @@
+package com.metlife.producer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            System.err.println("Let's inspect beans provided by Spring Boot");
+            Arrays.asList(context.getBeanDefinitionNames()).forEach(System.err::println);
+        };
+    }
+}
