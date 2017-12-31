49b344160df83ddb35c4024e8dc730e714b4dedf
Imran Khan <ikhan@thoughtworks.com>
2017-12-31 21:47:56 +1000

    [CD] Renamed cluster

--- scripts/gcloud/cleanup.sh
+++ scripts/gcloud/cleanup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcloud container clusters delete nahknarmi --zone=australia-southeast1-a
+gcloud container clusters delete dev-cluster --zone=australia-southeast1-a
--- scripts/gcloud/create-cluster.sh
+++ scripts/gcloud/create-cluster.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-gcloud container clusters create nahknarmi --num-nodes=3 --zone=australia-southeast1-a
+gcloud container clusters create dev-cluster --num-nodes=3 --zone=australia-southeast1-a
 kubectl run provider --image=asia.gcr.io/nahknarmi-190508/com.metlife.provider --port 8080
