54a5ee010017d29a163de3d05c68ef73ae6c4dba
Imran Khan <ikhan@thoughtworks.com>
2017-12-31 10:46:39 +1000

    [CD] Cluster create script also runs container

--- scripts/gcloud/create-cluster.sh
+++ scripts/gcloud/create-cluster.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 gcloud container clusters create nahknarmi --num-nodes=3 --zone=australia-southeast1-a
+kubectl run provider --image=asia.gcr.io/nahknarmi-190508/com.metlife.provider --port 8080
