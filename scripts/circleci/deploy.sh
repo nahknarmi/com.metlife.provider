08b269eb3f5a6e58a98bf7064da75a08c8f96917
Imran Khan <ikhan@thoughtworks.com>
2018-01-03 17:32:39 +1100

    [CD] Added smoketest and necessary endpoint to test

--- scripts/circleci/deploy.sh
+++ scripts/circleci/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl apply -f scripts/circleci/config/deployment-provider.yml
+#template substitution
+cat scripts/circleci/config/deployment-provider.yml | sed -e "s/\${GIT_HASH}/${CIRCLE_SHA1}/" > /tmp/deployment-provider.yml
+
+kubectl apply -f /tmp/deployment-provider.yml
 kubectl apply -f scripts/circleci/config/service-provider.yml
---
+++ scripts/circleci/smoke-test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+n=0
+until [ $n -ge 5 ]
+do
+  LOAD_BALANCER_IP=$(kubectl get services provider -o json | jq -r '.status.loadBalancer.ingress[0].ip')
+  echo ${LOAD_BALANCER_IP}
+
+  GIT_SHA1=$(curl ${LOAD_BALANCER_IP}/info | jq -r '.git.commit.id')
+  echo ${GIT_SHA1}
+  echo $(echo ${CIRCLE_SHA1} | cut -c -7)
+
+  if [ ${GIT_SHA1} = $(echo ${CIRCLE_SHA1} | cut -c -7) ]; then
+    exit 0
+  fi;
+  n=$[$n+1]
+  sleep 10
+done
+
+exit 1
